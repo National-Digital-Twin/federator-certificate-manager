4e128bed158e51bafc1af76260d3e73bed65cfee
kingarj <alice.king@informed.com>
2026-03-13 11:25:02 +0000

    feature: [DPAV-2628] add copyright

--- scripts/generate-bootstrap-cert.sh
+++ scripts/generate-bootstrap-cert.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# * SPDX-License-Identifier: Apache-2.0
+# * © Crown Copyright 2026. This work has been developed by the National Digital Twin Programme and is legally
+# * attributed to the Department for Business and Trade (UK) as the governing entity.
+#
 ###############################################################################
 # Bootstrap Certificate Test Utility
 #
